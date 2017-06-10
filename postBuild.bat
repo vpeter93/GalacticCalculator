1df14b08ad9723309f2607217b618579b01183ca
vpeter93 <vpeter93@hotmail.com>
2017-06-10 14:55:09 +0200

    add version

--- postBuild.bat
+++ postBuild.bat
@@ -1,2 +1,3 @@
 copy ..\build-GalacticCalculator-Desktop_Qt_5_8_0_MinGW_32bit-Release\release\GalacticCalculator.exe "..\GalacticCalculator-Release\GalacticCalculator.exe"
-"C:\Program Files\7-Zip\7z" a -tzip ..\GalacticCalculator-win32.zip ..\GalacticCalculator-Release\* 
+"C:\Program Files\7-Zip\7z" a -tzip ..\GalacticCalculator-win32.zip ..\GalacticCalculator-Release\*
+"C:\Program Files\7-Zip\7z" a -tzip E:\Dropbox\Tajo\GalacticCalculator-win32.zip ..\GalacticCalculator-Release
