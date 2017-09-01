e20bb61e57f5fdcf6ad110e20647e620e64817cd
vpeter93 <vpeter93@hotmail.com>
2017-09-01 13:03:33 +0200

    update gitignore

--- postBuild.bat
+++ postBuild.bat
@@ -1,4 +1,4 @@
 copy ..\build-GalacticCalculator-Desktop_Qt_5_8_0_MinGW_32bit-Release\release\GalacticCalculator.exe "..\GalacticCalculator-Release\GalacticCalculator.exe"
-copy ..\build-GalacticCalculator-Desktop_Qt_5_8_0_MinGW_32bit-Release\release\GalacticCalculator.exe "..\GalacticCalculator-ReleaseUpdate\GalacticCalculator.exe"
+copy ..\build-GalacticCalculator-Desktop_Qt_5_8_0_MinGW_32bit-Release\release\GalacticCalculator.exe "..\GalacticCalculator-ReleaseUpdate\GalacticCalculator-Release\GalacticCalculator.exe"
 "C:\Program Files\7-Zip\7z" a -tzip E:\Dropbox\Tajo\GalacticCalculator-win32Full.zip ..\GalacticCalculator-Release
 "C:\Program Files\7-Zip\7z" a -tzip E:\Dropbox\Tajo\GalacticCalculator-win32.zip ..\GalacticCalculator-ReleaseUpdate\GalacticCalculator-Release
